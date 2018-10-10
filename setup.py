3af7a534e7d2efdf6efc2ed89f730c9401aa4a4b
fmell <fmell@statoil.com>
2018-10-10 17:15:12 +0200

    add setup.py and command line entry point

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name = 'barcode',
+    version = '0.1.0',
+    packages = ['barcode'],
+    entry_points = {
+        'console_scripts': [
+            'barcode = barcode.__main__:main'
+        ]
+    })
